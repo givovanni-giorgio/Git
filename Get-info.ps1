e0cb4c98ecb151fd780bdd042b68a37168fd82b7
givovanni-giorgio <radekpawlowskinew@gmail.com>
2022-11-19 20:10:50 +0100

    Update index.html with a new line

--- Get-info.ps1
+++ Get-info.ps1
@@ -1,1 +1,5 @@
 $name = Get-ComputerInfo | Select-Object -Property TimeZone
+$processor = Get-CimInstance -ClassName Win32_BIOS | Select-Object -Property Caption
+
+Read-Host "Type your answer"
+Write-Host "What's up"
---
+++ README.md
@@ -0,0 +1,8 @@
+# Starting my jurney
+
+>Staging some files 
+>> Stagin README.md file
+>> Hope it will turn out well
+>> Hopefuly 
+>> Tracking changes
+
