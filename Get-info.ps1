2e3371f8faa851f7a9a9dd01e44d352858b96832
givovanni-giorgio <radekpawlowskinew@gmail.com>
2022-11-19 20:22:26 +0100

    I made some changes

--- Get-info.ps1
+++ Get-info.ps1
@@ -3,3 +3,6 @@ $processor = Get-CimInstance -ClassName Win32_BIOS | Select-Object -Property Cap
 
 Read-Host "Type your answer"
 Write-Host "What's up"
+
+Write-Host "Computer Name it is what inspires me."
+Write-Host "Computer name it is what i think it inspires me."
--- README.md
+++ README.md
@@ -5,4 +5,6 @@
 >> Hope it will turn out well
 >> Hopefuly 
 >> Tracking changes
-
+>> Follow your dreams
+>> you have to work hard in order to make it happen
+>> you will be fine
